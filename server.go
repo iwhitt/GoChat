bf1fa5b8c0cb79abed08db25d62ede45fcb216ad
iwhitt <iwhitt567@yahoo.com>
2017-04-26 15:57:48 -0400

    Pf

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"net"
 	"bufio"
 	"fmt"
+	"os"
 )
 
 type Client struct {
@@ -11,39 +12,83 @@ type Client struct {
 	//cc chan string
 	cr *bufio.Reader
 	cw *bufio.Writer
+	name string
 }
 
 func main() {
-	a, _ := net.ResolveTCPAddr("tcp", ":8765")
+	run := true
+
+	a, err := net.ResolveTCPAddr("tcp", ":8765")
+	if err!=nil {
+		fmt.Println("Error resolving address.")
+		run = false
+	}
 
 	l, _ := net.ListenTCP("tcp",a)
+	if err!=nil {
+		fmt.Println("Error listening.")
+		run = false
+	}
 
 	var cl []*Client
 
+	ic := make(chan string)		// Input channel
 	cc := make(chan string)		// Client channel
 	jc := make(chan *net.TCPConn)	// Join channel
+	lc := make(chan *Client)	// Leave channel
 
+	go inputListen(ic)
 	go joinListen(l, jc)
 
-	for {
+	for run{
 		select {
-		case conn := <- jc:
+		case cmd := <-ic:
+			if cmd=="quit" {
+				return
+			}
+		case conn := <-jc:
 			c := Client{conn,
 				    bufio.NewReader(conn),
-				    bufio.NewWriter(conn)}
+				    bufio.NewWriter(conn),
+				    "NONAME"}
 			cl = append(cl, &c)
-			go clientListen(c, cc)
-		case str := <- cc:
+			go clientListen(c, cc, lc)
+		case str := <-cc:
 			for _, c := range cl {
 				c.cw.WriteString(str)
 				c.cw.Flush()
 			}
 			fmt.Println(str)
+		case c := <- lc:
+			for i, tc := range cl {
+				if c==tc {
+					cl = append(cl[:i], cl[i+1:]...)
+					fmt.Println("Client removed.")
+				}
+			}
 		}
 	}
 }
 
+func inputListen(ic chan string) {
+	r := bufio.NewReader(os.Stdin)
+	for {
+		str, err := r.ReadString('\n')
+		if err!= nil {
+			fmt.Println("Error reading from console.")
+			continue
+		}
+		if str=="quit" {
+			fmt.Println("QUIT")
+			ic <- str
+			break
+		}
+		ic <- str
+	}
+}
+
 func joinListen(l *net.TCPListener, jc chan *net.TCPConn) {
+	fmt.Println("Listening...")
 	for {
 		conn, err := l.AcceptTCP()
 		if err!=nil {
@@ -55,13 +100,25 @@ func joinListen(l *net.TCPListener, jc chan *net.TCPConn) {
 	}
 }
 
-func clientListen(c Client, cc chan string) {
+func clientListen(c Client, cc chan string, lc chan *Client) {
 	for {
 		str, err := c.cr.ReadString('\n')
 		if err!= nil {
 			fmt.Println("Error reading from client. Dropping client.")
+			lc <- &c
 			break
 		}
-		cc <- str
+		tag := string(str[0])
+		mes := string(str[1:])
+		if tag=="0" {
+			c.name = mes
+		} else if tag=="1" {
+			mes = c.name + ": " + mes
+			cc <- mes
+		} else if tag=="2" {
+			fmt.Println("Client",c.name,"quit.")
+			lc <- &c
+			break
+		}
 	}
 }
